5ee4955cea7cba73cc8c100ae0b0672d93c3d996
Eray Yuksel <yukseleray203@gmail.com>
2023-09-10 16:30:09 +0300

    Added Scene Persist

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     [SerializeField] AudioClip coinPickupSFX;
-    private void OnTriggerEnter2D(Collider2D collision)
+    [SerializeField] int pointsForCoinPickup = 100;
+    //bool wasCollected = false;
+    private void OnTriggerEnter2D(Collider2D collision) //yorum satirindakiler coin topladiginda scora 2 kere ekleme yapmasin diye sonradan yazdik 
     {
         if (collision.tag == "Player")
         {
+            //wasCollected = true;
+            FindObjectOfType<GameSession>().addToScore(pointsForCoinPickup); // her bir coin playerla temas ettiginde bu fonksiyon calisir ve score artar
             AudioSource.PlayClipAtPoint(coinPickupSFX, Camera.main.transform.position); // cameranin konumunda ses calisacak 
+            //gameObject.SetActive(false);
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/Game Session.cs
+++ Assets/Scripts/Game Session.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameSession : MonoBehaviour
 {
     [SerializeField] float playerLives = 3;
+    [SerializeField] float score = 0;
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
     void Awake()
     {
         int numaGameSesions = FindObjectsOfType<GameSession>().Length;
@@ -18,6 +22,11 @@ public class GameSession : MonoBehaviour
         {
             DontDestroyOnLoad(gameObject);
         }
+    }
+    private void Start()
+    {
+        livesText.text= playerLives.ToString();
+        scoreText.text= score.ToString();
     }
     public void ProcessPlayerDeath() // elemanin canini genel kontrol saglayan fonksiyon 
     {
@@ -36,11 +45,20 @@ public class GameSession : MonoBehaviour
         playerLives--;
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        livesText.text = playerLives.ToString(); // bu texte yazdirma islemi burda olmali cunku playerMovement taki die fonksiyonu calistiginda bu da calisir 
+
     }
 
     void ResetGameSession()
     {
         SceneManager.LoadScene(0);
         Destroy(gameObject); // bu objeyi yok etmem gerekki hersey 0 dan baslasin 
+    }
+    public void addToScore(int pointsToAdd)
+    {
+        score += pointsToAdd;
+        scoreText.text= score.ToString();
+
+
     }
 }
