46b4ab167221d6636bc9c245f0c1c662932d3d95
Eray Yuksel <yukseleray203@gmail.com>
2023-09-10 11:11:07 +0300

    Level exit portal added

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 1f;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; //build settingsde kenarlarinda index numaralari yaziyor 
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings) //next olan toplam sahne sayima esitlendiyse 0 la bastan baslasin cunku sonraki level olmadigi icin hata verir 
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex); // temas oldugunda bir sonraki leveli yukler 
+
+    }
+}
